188a6293a6677b1bd472ce81b85d9bad8188df12
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-11-09 05:47:45 -0800

    Add files via upload

---
+++ days30_dictionary.py
@@ -0,0 +1,18 @@
+#dictionary
+#ketika di list array  kita mengakses data dengan index misalnya
+a = [1,2,3,4]
+print (a[3])
+
+#dengan dictionary kita mengakses data dengan assosiative array
+#contohnya
+b = {
+    "nama":"rahmawati",
+    "prodi":" teknik informatika"
+    }
+print (b)
+#cara memanggilnya ketika kita ingin mengakses  elemen saja kita tidak perlu menggunkan indexnya lgi
+b = {
+    "nama":"rahmawati",
+    "prodi":" teknik informatika"
+    }
+print(b['nama'])
